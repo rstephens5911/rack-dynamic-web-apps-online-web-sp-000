473697503ebda5accda8171ef1974a82824d2927
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-18 23:22:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,11 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish
+    
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
+    
   end
 
 end
